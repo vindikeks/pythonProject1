73712b4fefcbc2cf80d0ea4924c3ac320be65173
vindikeks <misha.gordeico@gmail.com>
2023-05-10 11:34:42 +0300

    dell !!!

--- Leetcode/ret.py
+++
@@ -1,7 +0,0 @@
-import os
-import time
-
-source = ['"C:\\My Documents"', 'C:\\Code']
-target_dir = 'E:\\Backup'
-copy_path = ["D:\BACK"]
-today = target_dir + time.strftime('%Y%m%d%H%M%S') + '.zip'
